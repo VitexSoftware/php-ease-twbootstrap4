86dcc423a7f05dad7dbbb89ff83b9c90753eed79
Cybervitexus <info@vitexsoftware.cz>
2019-06-10 22:57:11 +0200

    LinkButton & Submit Button added

---
+++ src/Ease/TWB4/SubmitButton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ease\TWB4;
+
+/**
+ * Odesílací tlačítko formuláře Twitter Bootstrapu.
+ */
+class SubmitButton extends \Ease\Html\ButtonTag
+{
+
+    /**
+     * Odesílací tlačítko formuláře Twitter Bootstrapu.
+     *
+     * @param string $value vracená hodnota
+     * @param string $type  primary|info|success|warning|danger|inverse|link
+     */
+    public function __construct($value = null, $type = null, $properties = [])
+    {
+        if (is_null($type)) {
+            $properties['class'] = 'btn';
+        } else {
+            $properties['class'] = 'btn btn-'.$type;
+        }
+        parent::__construct($value, $properties);
+        Part::twBootstrapize();
+    }
+}
