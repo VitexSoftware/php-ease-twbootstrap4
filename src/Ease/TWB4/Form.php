41fc39bf7df404642d708c80f646dfcfedf8417a
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-06-12 15:41:21 +0200

    Form/FormGroup added

---
+++ src/Ease/TWB4/Form.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Formulář Bootstrapu.
+ */
+
+namespace Ease\TWB4;
+
+class Form extends \Ease\Html\Form
+{
+
+    /**
+     * Vloží prvek do formuláře.
+     *
+     * @param mixed  $input       Vstupní prvek
+     * @param string $caption     Popisek
+     * @param string $placeholder předvysvětlující text
+     * @param string $helptext    Dodatečná nápověda
+     */
+    public function addInput($input, $caption = null, $placeholder = null,
+                             $helptext = null)
+    {
+        return $this->addItem(new FormGroup($caption, $input, $placeholder,
+                    $helptext));
+    }
+
+    /**
+     * Vloží další element do formuláře a upraví mu css.
+     *
+     * @param mixed  $pageItem     hodnota nebo EaseObjekt s metodou draw()
+     * @param string $pageItemName Pod tímto jménem je objekt vkládán do stromu
+     *
+     * @return pointer Odkaz na vložený objekt
+     */
+    public function &addItem($pageItem, $pageItemName = null)
+    {
+        if (is_object($pageItem) && method_exists($pageItem, 'setTagClass')) {
+            if (strtolower($pageItem->tagType) == 'select') {
+                $pageItem->setTagClass(trim(str_replace('form_control', '',
+                            $pageItem->getTagClass().' form-control')));
+            } elseif ($pageItem->getTagProperty('type') == 'file') {
+                $pageItem->setTagClass('form-control-file');
+            }
+        }
+        $added = parent::addItem($pageItem, $pageItemName);
+
+        return $added;
+    }
+}
---
+++ src/Ease/TWB4/FormGroup.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ease\TWB4;
+
+class FormGroup extends \Ease\Html\DivTag
+{
+
+/**
+ * Položka TWBootstrp formuláře.
+ *
+ * @param string         $label       popisek pole formuláře
+ * @param \Ease\Html\Tag $content     widget formuláře
+ * @param string         $placeholder předvysvětlující text
+ * @param string         $helptext    Nápvěda pod prvkem
+ * @param string         $addTagClass CSS třída kterou má být oskiován vložený prvek
+ */
+    public function __construct($label = null, $content = null,
+                                $placeholder = null, $helptext = null,
+                                $addTagClass = 'form-control')
+    {
+        if (is_object($content) && method_exists($content, 'getTagID')) {
+            $id = $content->getTagID();
+        } else {
+            $id = null;
+        }
+        if (is_null($id)) {
+            $formKey = \Ease\Brick::lettersOnly($label);
+        } else {
+            $formKey = $id;
+        }
+        $properties['class'] = 'form-group';
+        parent::__construct(null, $properties);
+        $this->addItem(new \Ease\Html\LabelTag($formKey, $label));
+        $content->addTagClass($addTagClass);
+        if ($placeholder) {
+            $content->setTagProperties(['placeholder' => $placeholder]);
+        }
+        $content->setTagId($formKey);
+        $this->addItem($content);
+        if ($helptext) {
+            $this->addItem(new \Ease\Html\PTag($helptext,
+                    ['class' => 'help-block']));
+        }
+    }
+}
