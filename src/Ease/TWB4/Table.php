ae9ace0a934937c67fb6de6c89e8bd76008cbfc1
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-07-18 23:38:37 +0200

    Basic Table support added

---
+++ src/Ease/TWB4/Table.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Ease\TWB4;
+
+/**
+ * Description of Table
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+class Table extends \Ease\Html\TableTag
+{
+
+    /**
+     * TWB4 Table.
+     *
+     * @param mixed $content    tag content
+     * @param array $properties table tag options
+     */
+    public function __construct($content = null, $properties = [])
+    {
+        parent::__construct($properties, $content);
+        $this->addTagClass('table');
+    }
+}
