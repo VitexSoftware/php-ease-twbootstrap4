d41f86216bb210ef6b835fa8f26b9bb17330bd51
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-03-15 20:36:46 +0100

    CheckBox added

---
+++ src/Ease/TWB4/Checkbox.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Checkbox pro TwitterBootstrap4.
+ */
+
+namespace Ease\TWB4;
+
+class Checkbox extends \Ease\Html\DivTag
+{
+    /**
+     * Odkaz na checkbox.
+     *
+     * @var \Ease\Html\CheckboxTag
+     */
+    public $checkbox = null;
+
+    /**
+     * Checkbox pro TwitterBootstrap4.
+     *
+     * @param string     $name
+     * @param string|int $value
+     * @param mixed      $content
+     * @param bool       $checked
+     * @param array      $properties
+     */
+    public function __construct($name = null, $value = 'on', $content = null,
+                                $checked = false, $properties = [])
+    {
+        parent::__construct(null,['class'=>'form-check']);
+        $this->checkbox = $this->addItem( new \Ease\Html\CheckboxTag($name, $checked, $value, $properties));
+        $this->checkbox->setTagID($name);
+        $this->addItem(new \Ease\Html\LabelTag($this->checkbox->getTagID(), $content )) ;
+    }
+}
