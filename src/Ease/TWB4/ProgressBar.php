63b8d270993863d8ad3a82be733a7015aeafae19
CyberVitexus <info@vitexsoftware.cz>
2024-08-25 11:56:42 +0200

    support more options for ProgressBar

--- src/Ease/TWB4/ProgressBar.php
+++ src/Ease/TWB4/ProgressBar.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace Ease\TWB4;
+
 /**
  * Twitter Bootstrap4 Alert.
  *
  * @author    Vitex <vitex@hippy.cz>
  * @copyright 2024 Vitex@hippy.cz (G)
  */
+class ProgressBar extends \Ease\Html\DivTag {
 
-class ProgressBar extends \Ease\Html\DivTag
-{
     /**
      *
      * @var int
@@ -23,19 +23,34 @@ class ProgressBar extends \Ease\Html\DivTag
     public $max = 100;
 
     /**
-     *
-     * @var int
+     * ProgressBar
+     * 
+     * @param int     $value  0-100
+     * @param string  $label  progress bar caption
+     * @param string  $tweaks progress-bar-striped progress-bar-animated bg-info
+     * @param array   $properties of main div of progress bar
      */
-    public function __construct($value = 0, $label = '', $properties = [])
-    {
+    public function __construct($value = 0, $label = '', $tweaks = '', $properties = []) {
         $properties['class'] = 'progress';
-        parent::__construct(new \Ease\Html\DivTag($label, [
-                    'class' => 'progress-bar',
-                    'role' => 'progressbar',
-                    'aria-valuenow' => $value,
-                    'aria-valuemin' => $this->min,
-                    'aria-valuemax' => $this->max,
-                    'style' => 'width: ' . $value . '%'
-                        ]), $properties);
+        parent::__construct($this->bar($value, $label, $tweaks), $properties);
+    }
+
+    /**
+     * ProgressBar core
+     * 
+     * @param string $label
+     * @param string $tweaks
+     * 
+     * @return \Ease\Html\DivTag
+     */
+    public function bar($value, $label = '', $tweaks = '') {
+        return new \Ease\Html\DivTag($label, [
+            'class' => 'progress-bar ' . $tweaks,
+            'role' => 'progressbar',
+            'aria-valuenow' => $value,
+            'aria-valuemin' => $this->min,
+            'aria-valuemax' => $this->max,
+            'style' => 'width: ' . $label . '%'
+        ]);
     }
 }
