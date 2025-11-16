e684b25e454d69ed23b4f48c6567b6b634849fa5
Vítězslav Dvořák <info@vitexsoftware.cz>
2025-11-16 22:19:24 +0100

    Initial CardBody class added

---
+++ src/Ease/TWB4/CardBody.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the EaseTWBootstrap4 package
+ *
+ * https://github.com/VitexSoftware/php-ease-twbootstrap4
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ease\TWB4;
+
+use Ease\Html\DivTag;
+
+/**
+ * Description of Card Body
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+class Card extends DivTag
+{
+    /**
+     * Card.
+     *
+     * @param string                $content
+     * @param array<string, string> $properties
+     */
+    public function __construct($content = null, array $properties = [])
+    {
+        if (\array_key_exists('class', $properties)) {
+            $properties['class'] = 'card-body '.$properties['class'];
+        } else {
+            $properties['class'] = 'card-body';
+        }
+
+        parent::__construct($content, $properties);
+    }
+}
