cab5cbb061db58b81cc89cd7c9b173dcc21130ef
CyberVitexus <info@vitexsoftware.cz>
2026-03-10 00:02:07 +0100

    Update autoload.php to include EaseHtml autoloader and refine class loading for Ease\\TWB4 namespace

--- debian/autoload.php
+++ debian/autoload.php
@@ -1,12 +1,15 @@
 <?php
 // autoload.php for EaseTWB4
 
+require_once '/usr/share/php/EaseHtml/autoload.php';
+
 // Register the autoloader for the EaseTWB4 library
 spl_autoload_register(function ($class) {
-    // Only autoload classes from the Ease namespace
-    if (strpos($class, 'Ease\\') === 0) {
-        $baseDir = __DIR__ . '/Ease/';
-        $relativeClass = substr($class, strlen('Ease\\'));
+    // Only autoload classes from the Ease\\TWB4 namespace
+    $prefix = 'Ease\\TWB4\\';
+    if (strpos($class, $prefix) === 0) {
+        $baseDir = __DIR__ . '/';
+        $relativeClass = substr($class, strlen($prefix));
         $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
         if (file_exists($file)) {
             require $file;
